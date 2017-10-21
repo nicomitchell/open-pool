14aab5ec6b187d870b3bd3d0c7e9df09837fda45
nicomitchell <namitc02@louisville.edu>
2017-10-21 14:39:39 -0400

    python capital one account creation scripts

--- c1API_tests/create_main_account.py
+++ c1API_tests/create_main_account.py
@@ -1,15 +1,20 @@
 import json
 import requests
 
-customer_key = '59eb784cb390353c953a1554'
+customer_key = '59eb8bffb390353c953a1561'
 api_key = 'a1e0eb837b518f02a40fb0319ba0c776'
 url = 'http://api.reimaginebanking.com/customers/{}/accounts?key={}'.format(customer_key,api_key)
 
+
+a_type = str(input("Account type: "))
+a_name = str(input("Account name: "))
+a_rewards = int(input("Rewards: "))
+a_balance = int(input("Balance: "))
 account = {
-  "type": "Checking",
-  "nickname": "Company account",
-  "rewards": 0,
-  "balance": 200000,	
+  "type": a_type,
+  "nickname": a_name,
+  "rewards": a_rewards,
+  "balance": a_balance
 }
 
 response = requests.post( 
